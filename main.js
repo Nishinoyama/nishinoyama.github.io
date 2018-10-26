81b63aa494e5a56391d4333a4d15dc35873252a5
Nishinoyama <begoingto1414@gmail.com>
2018-10-27 01:04:33 +0900

    new function

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 
 
-var x = 0;
-document.write(x);
+function atx(){
+
+    let out = document.formal.output1.value;
+
+    alert(out);
+    if( out == "114514" ){
+        alert("いいよこいよむねにかけてむねに");
+    }
+
+    if( isNaN(out) ) return;
+    let snnn = false;
+    let snnnn = "37";
+    let snnnnn = "SN";
+    for( let c = 0; c < 10000; c++ ){
+        snnnn += "7";
+        snnnnn += "N";
+        if( out == snnnn ) {
+            snnn = true;
+            break;
+        }
+    }
+    if( snnn ) alert( snnnnn + "姉貴すき");
+
+
+}
+
+let x = 0;
+
+
+
 
 
