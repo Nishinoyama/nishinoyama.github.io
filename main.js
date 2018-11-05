dd36759aee06b111bd84526831ff23089740beeb
Nishinoyama <begoingto1414@gmail.com>
2018-11-05 16:15:10 +0900

    add response

--- main.js
+++ main.js
@@ -5,14 +5,20 @@ function atx(){
     let out = document.formal.output1.value;
 
     alert(out);
+    //きたない
     if( out == "114514" ){
         alert("いいよこいよむねにかけてむねに");
     }
 
-    if( isNaN(out) ) return;
+    //みろよみろよ
+    if( out == "364364" ){
+        alert("ほら、みろよみろよ、見とけよ見とけよ〜")
+    }
+
     let snnn = false;
     let snnnn = "37";
     let snnnnn = "SN";
+    //37以上のSNNN数に反応
     for( let c = 0; c < 10000; c++ ){
         snnnn += "7";
         snnnnn += "N";
